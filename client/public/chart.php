3384b82123b2d96d833a4c0f809ae0d384d2535f
Phuong Bui <phuong.bui@go1.com.au>
2018-11-26 16:44:41 +0700

    add chart

---
+++ client/public/chart.php
@@ -0,0 +1,29 @@
+<div class="tradingview-widget-container">
+    <div id="tradingview_78453"></div>
+    <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BINANCE-ADAUSDT/" rel="noopener" target="_blank"><span class="blue-text">ADAUSDT chart</span></a> by TradingView</div>
+    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+    <script type="text/javascript">
+        new TradingView.widget(
+            {
+                "width": 980,
+                "height": 610,
+                "symbol": "BINANCE:ADAUSDT",
+                "interval": "H",
+                "timezone": "Etc/UTC",
+                "theme": "Light",
+                "style": "1",
+                "locale": "en",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "allow_symbol_change": true,
+                "studies": [
+                    "BB@tv-basicstudies"
+                ],
+                "container_id": "tradingview_78453"
+            }
+        );
+    </script>
+</div>
+
+<?php
+
